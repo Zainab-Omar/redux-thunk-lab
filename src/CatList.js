6e024182796eae684b4fd092aba0d4cb5c8d1dc4
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-20 14:20:12 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,13 @@
-// write your CatList component here
+// import { render } from 'enzyme';
+import React from 'react'
+
+const CatList = props => {
+    return(
+        <div>
+         {props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />)}
+        </div>
+    )
+}
+
+
+export default CatList;
